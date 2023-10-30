2c8425a89c36d9e828d7ab562a330fc50c9557f0
cocoon-projet <contact@cocoon-projet.fr>
2023-10-30 17:39:07 +0100

    update test operator

--- tests/CollectionTest.php
+++ tests/CollectionTest.php
@@ -279,6 +279,22 @@ class CollectionTest extends \PHPUnit\Framework\TestCase
             3 =>  ['id' => 103, 'product' => 'Samsung TV', 'price' => 500, 'quantite' => 35],
             2  => ['id' => 102, 'product' => 'Sony TV', 'price' => 865, 'quantite' => 9]
         ], $test->where('price', '>=', 500)->toArray());
+        $this->assertEquals([
+            1 =>  ['id' => 101, 'product' => 'Play Station', 'price' => 500, 'quantite' => 35],
+            3 =>  ['id' => 103, 'product' => 'Samsung TV', 'price' => 500, 'quantite' => 35],
+        ], $test->where('price', '===', 500)->toArray());
+        $this->assertEquals([
+            0 =>  ['id' => 100, 'product' => 'Play Station', 'price' => 450, 'quantite' => 35],
+            2  => ['id' => 102, 'product' => 'Sony TV', 'price' => 865, 'quantite' => 9]
+        ], $test->where('price', '!==', 500)->toArray());
+        $this->assertEquals([
+            0 =>  ['id' => 100, 'product' => 'Play Station', 'price' => 450, 'quantite' => 35],
+            2  => ['id' => 102, 'product' => 'Sony TV', 'price' => 865, 'quantite' => 9]
+        ], $test->where('price', '<>', 500)->toArray());
+        $this->assertEquals([
+            0 =>  ['id' => 100, 'product' => 'Play Station', 'price' => 450, 'quantite' => 35],
+            2  => ['id' => 102, 'product' => 'Sony TV', 'price' => 865, 'quantite' => 9]
+        ], $test->where('price', '!=', 500)->toArray());
     }
     // whereIn
     public function testWhereInCollection()
